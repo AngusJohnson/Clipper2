b2c1f315db3c771f798afbcae474ea6b6ca22f78
angusj <angus@angusj.com>
2022-04-07 14:59:46 +1000

    Clipper2LibExample tidy

--- CSharp/Clipper2LibExample/Program.cs
+++ CSharp/Clipper2LibExample/Program.cs
@@ -6,6 +6,24 @@ namespace Clipper2LibExample
   using Paths64 = List<List<Point64>>;
   internal class Program
   {
+
+    internal static void MakeSvg(string caption, string filename,
+      Paths64 subj, Paths64 openSubj, Paths64 clip,
+      Paths64 solution, FillRule fill)
+    {
+      SimpleClipperSvgWriter svg = new SimpleClipperSvgWriter(fill);
+      svg.AddText(caption, 0, 25, 14);
+      if (subj != null)
+        svg.AddPaths(subj, false, 0x110066FF, 0x33000099, 0.8);
+      if (openSubj != null)
+        svg.AddPaths(openSubj, true, 0x110066FF, 0x33000099, 1.8);
+      if (clip != null)
+        svg.AddPaths(clip, false, 0x11996600, 0x55993300, 0.8);
+      if (solution != null)
+        svg.AddPaths(solution, false, 0x4000FF00, 0x80000000, 1.2, true);
+      svg.SaveToFile(filename, 800, 600);
+    }
+
     static void Main(string[] args)
     {
       Paths64 subj = new Paths64
@@ -20,78 +38,43 @@ namespace Clipper2LibExample
       SimpleClipperSvgWriter svg = new SimpleClipperSvgWriter(FillRule.EvenOdd);
 
       Paths64 solution1 = ClipperFunc.Union(subj, null, FillRule.EvenOdd);
-      svg.FillRule = FillRule.EvenOdd;
-      svg.AddText("Sample 1 - union; even", 0, 0, 14);
-      svg.AddPaths(subj, false, 0x110066FF, 0x33000099, 0.8);
-      svg.AddPaths(solution1, false, 0x4000FF00, 0x80000000, 1.2, true);
-      svg.SaveToFile("../../../solution1.svg", 800, 600);
+      MakeSvg("Sample 1 - union; even", "../../../solution1.svg",
+        subj, null, null, solution1, FillRule.EvenOdd);
 
       Paths64 solution2 = ClipperFunc.Union(subj, null, FillRule.NonZero);
-      svg.ClearAll();
-      svg.FillRule = FillRule.NonZero;
-      svg.AddText("Sample 2 - union; nonzero", 0, 0, 14);
-      svg.AddPaths(subj, false, 0x110066FF, 0x33000099, 0.8);
-      svg.AddPaths(solution2, false, 0x4000FF00, 0x80000000, 1.2, true);
-      svg.SaveToFile("../../../solution2.svg", 800, 600);
+      MakeSvg("Sample 2 - union; nonzero", "../../../solution2.svg",
+        subj, null, null, solution2, FillRule.NonZero);
 
       Paths64 solution3 = ClipperFunc.Union(subj, clip, FillRule.NonZero);
-      svg.ClearAll();
-      svg.FillRule = FillRule.NonZero;
-      svg.AddText("Sample 3 - union; nonzero", 0, 0, 14);
-      svg.AddPaths(subj, false, 0x110066FF, 0x33000099, 0.8);
-      svg.AddPaths(clip, false, 0x11996600, 0x55993300, 0.8);
-      svg.AddPaths(solution3, false, 0x4000FF00, 0x80000000, 1.2, true);
-      svg.SaveToFile("../../../solution3.svg", 800, 600);
+      MakeSvg("Sample 3 - union; nonzero", "../../../solution3.svg",
+        subj, null, clip, solution3, FillRule.NonZero);
 
       Paths64 solution4 = ClipperFunc.Intersect(subj, clip, FillRule.NonZero);
-      svg.ClearAll();
-      svg.FillRule = FillRule.NonZero;
-      svg.AddText("Sample 4 - intersect; nonzero", 0, 0, 14);
-      svg.AddPaths(subj, false, 0x110066FF, 0x33000099, 0.8);
-      svg.AddPaths(clip, false, 0x11996600, 0x55993300, 0.8);
-      svg.AddPaths(solution4, false, 0x4000FF00, 0x80000000, 1.2, true);
-      svg.SaveToFile("../../../solution4.svg", 800, 600);
+      MakeSvg("Sample 4 - intersect; nonzero", "../../../solution4.svg",
+        subj, null, clip, solution4, FillRule.NonZero);
 
       Paths64 solution5 = ClipperFunc.Intersect(subj, clip, FillRule.EvenOdd);
-      svg.ClearAll();
-      svg.FillRule = FillRule.EvenOdd;
-      svg.AddText("Sample 5 - intersect; evenodd", 0, 0, 14);
-      svg.AddPaths(subj, false, 0x110066FF, 0x33000099, 0.8);
-      svg.AddPaths(clip, false, 0x11996600, 0x55993300, 0.8);
-      svg.AddPaths(solution5, false, 0x4000FF00, 0x80000000, 1.2, true);
-      svg.SaveToFile("../../../solution5.svg", 800, 600);
+      MakeSvg("Sample 5 - intersect; evenodd", "../../../solution5.svg",
+        subj, null, clip, solution5, FillRule.EvenOdd);
 
       subj.Clear();
       subj.Add(ClipperFunc.MakePath(new int[] { 93, 50, 77, 84, 40, 92, 11, 69, 11, 31, 40, 8, 77, 16 }));
 
       Paths64 solution6 = ClipperFunc.InflatePaths(subj, 5.0, JoinType.Miter, EndType.Polygon);
-      svg.ClearAll();
-      svg.AddText("Sample 6 - inflate:+5; end:polygon", 0, 20, 14);
-      svg.AddPaths(subj, false, 0x110066FF, 0x33000099, 1.8);
-      svg.AddPaths(solution6, false, 0x4000FF00, 0x80000000, 1.2, true);
-      svg.SaveToFile("../../../solution6.svg", 800, 600);
+      MakeSvg("Sample 6 - inflate:+5; end:polygon", "../../../solution6.svg",
+        subj, null, null, solution6, FillRule.EvenOdd);
 
       Paths64 solution7 = ClipperFunc.InflatePaths(subj, -5.0, JoinType.Miter, EndType.Polygon);
-      svg.ClearAll();
-      svg.AddText("Sample 7 - inflate:-5; end:polygon", 0, 20, 14);
-      svg.AddPaths(subj, false, 0x110066FF, 0x33000099, 0.8);
-      svg.AddPaths(solution7, false, 0x4000FF00, 0x80000000, 1.2, true);
-      svg.SaveToFile("../../../solution7.svg", 800, 600);
-
+      MakeSvg("Sample 7 - inflate:-5; end:polygon", "../../../solution7.svg",
+        subj, null, null, solution7, FillRule.EvenOdd);
 
       Paths64 solution8 = ClipperFunc.InflatePaths(subj, 10.0, JoinType.Miter, EndType.Square);
-      svg.ClearAll();
-      svg.AddText("Sample 8 - inflate:10; end:square", 0, 20, 14);
-      svg.AddPaths(subj, true, 0x110066FF, 0x33000099, 1.8);
-      svg.AddPaths(solution8, false, 0x4000FF00, 0x80000000, 1.2, true);
-      svg.SaveToFile("../../../solution8.svg", 800, 600);
+      MakeSvg("Sample 8 - inflate:10; end:square", "../../../solution8.svg",
+        null, subj, null, solution8, FillRule.EvenOdd);
 
       Paths64 solution9 = ClipperFunc.InflatePaths(subj, 10.0, JoinType.Miter, EndType.Joined);
-      svg.ClearAll();
-      svg.AddText("Sample 9 - inflate:10; end:joined", 0, 20, 14);
-      svg.AddPaths(subj, true, 0x110066FF, 0x33000099, 1.8);
-      svg.AddPaths(solution9, false, 0x4000FF00, 0x80000000, 1.2, true);
-      svg.SaveToFile("../../../solution9.svg", 800, 600);
+      MakeSvg("Sample 9 - inflate:10; end:joined", "../../../solution9.svg",
+        null, subj, null, solution9, FillRule.EvenOdd);
 
     }
   }
