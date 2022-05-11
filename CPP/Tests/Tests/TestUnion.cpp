e4f743c75fd63770abef848a026de1d993eed036
Juha Reunanen <juha.reunanen@tomaattinen.com>
2022-05-11 10:59:56 +0300

    Add very basic tests for Visual Studio

---
+++ CPP/Tests/Tests/TestUnion.cpp
@@ -0,0 +1,31 @@
+#include <gtest/gtest.h>
+#include "../../Clipper2Lib/clipper.h"
+
+TEST(Clipper2Tests, TestBasicUnion) {
+    Clipper2Lib::Clipper clipper;
+
+    const Clipper2Lib::Path64 a = {
+        Clipper2Lib::Point64(0, 0),
+        Clipper2Lib::Point64(0, 5),
+        Clipper2Lib::Point64(5, 5),
+        Clipper2Lib::Point64(5, 0)
+    };
+
+    const Clipper2Lib::Path64 b = {
+        Clipper2Lib::Point64(1, 1),
+        Clipper2Lib::Point64(1, 6),
+        Clipper2Lib::Point64(6, 6),
+        Clipper2Lib::Point64(6, 1)
+    };
+
+    clipper.AddSubject({ a, b });
+
+    Clipper2Lib::PolyTree64 solution;
+    Clipper2Lib::Paths64 open_paths;
+
+    clipper.Execute(Clipper2Lib::ClipType::Union, Clipper2Lib::FillRule::Positive, solution, open_paths);
+
+    EXPECT_EQ(open_paths.size(), 0);
+    ASSERT_EQ(solution.ChildCount(), 1);
+    EXPECT_EQ(solution.childs.front()->Path().size(), 8);
+}
