8e9424ae52074e829a898852f182085f9a6b9a97
Juha Reunanen <juha.reunanen@tomaattinen.com>
2022-05-12 01:30:19 +0300

    Add minimal CI test suite (#44)
    * Add very basic tests for Visual Studio
    
    * Add GitHub Actions script
    
    * Add GitHub Actions status badge
    
    * Treat warnings as errors

---
+++ CPP/Tests/Tests/TestIntersection.cpp
@@ -0,0 +1,32 @@
+#include <gtest/gtest.h>
+#include "../../Clipper2Lib/clipper.h"
+
+TEST(Clipper2Tests, TestBasicIntersection) {
+    Clipper2Lib::Clipper clipper;
+
+    const Clipper2Lib::Path64 a = {
+        Clipper2Lib::Point64(0, 0),
+        Clipper2Lib::Point64(0, 5),
+        Clipper2Lib::Point64(5, 5),
+        Clipper2Lib::Point64(5, 0)
+    };
+
+    const Clipper2Lib::Path64 b = {
+        Clipper2Lib::Point64(1, 1),
+        Clipper2Lib::Point64(1, 6),
+        Clipper2Lib::Point64(6, 6),
+        Clipper2Lib::Point64(6, 1)
+    };
+
+    clipper.AddSubject({ a });
+    clipper.AddClip   ({ b });
+
+    Clipper2Lib::PolyTree64 solution;
+    Clipper2Lib::Paths64 open_paths;
+
+    clipper.Execute(Clipper2Lib::ClipType::Intersection, Clipper2Lib::FillRule::Positive, solution, open_paths);
+
+    EXPECT_EQ(open_paths.size(), 0);
+    ASSERT_EQ(solution.ChildCount(), 1);
+    EXPECT_EQ(solution.childs.front()->Path().size(), 4);
+}
