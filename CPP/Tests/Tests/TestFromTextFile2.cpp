b8775ad47f716d4e48488de94ca5c3abfaecd992
Juha Reunanen <juha.reunanen@tomaattinen.com>
2022-06-05 21:03:18 +0300

    Add null pointer example

--- CPP/Tests/Tests/TestFromTextFile2.cpp
+++ CPP/Tests/Tests/TestFromTextFile2.cpp
@@ -8,12 +8,13 @@ TEST(Clipper2Tests, TestFromTextFile2) {
     ASSERT_TRUE(ifs.good());
         
     Clipper2Lib::Paths64 subject, subject_open, clip;
-    Clipper2Lib::Paths64 solution, solution_open;
+    Clipper2Lib::Paths64 solution_open;
+    Clipper2Lib::PolyTree64 solution;
     Clipper2Lib::ClipType ct;
     Clipper2Lib::FillRule fr;
     int64_t area, count;
 
-    for (auto test_number : { 1, 2 })
+    for (auto test_number : { 1, 2, 3 })
     {
         bool success = false;
         if (LoadTestNum(ifs, test_number, false, subject, subject_open, clip, area, count, ct, fr))
