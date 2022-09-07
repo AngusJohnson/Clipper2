e9b9ac74a0c9f8043fb2d5cc0e4b1eff2feae8a5
Nicholas Lederer <nlederer@dyndrite.com>
2022-09-06 19:39:49 -0700

    #171 add rudimentary documentation to CPP/cmake/win64

--- CPP/cmake/win64/win64_vs2019_solution.bat
+++ CPP/cmake/win64/win64_vs2019_solution.bat
@@ -3,11 +3,11 @@ setlocal enabledelayedexpansion
 
 pushd "%~dp0"
 
-md ..\..\build
+md ..\..\build_vs2019
 
-pushd "..\..\build"
+pushd "..\..\build_vs2019"
 
-cmake ../ -G "Visual Studio 16 2019" -A x64 
+cmake ../ -G "Visual Studio 16 2019" -A x64 -DCMAKE_INSTALL_PREFIX:PATH="%~dp0\..\..\install"
 
 popd
 popd
