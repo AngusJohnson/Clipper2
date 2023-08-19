a3fd0e6f1b23ec9af33b6121b2a80b5e5442e2b0
gigglesun <gigglesun@163.com>
2023-08-19 18:01:39 +0800

    Fix RectClip when rect clip the hole (#619)

---
+++ CPP/out/install/x64-Release/lib/cmake/GTest/GTestTargets-release.cmake
@@ -0,0 +1,29 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "GTest::gtest" for configuration "Release"
+set_property(TARGET GTest::gtest APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(GTest::gtest PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/gtest.lib"
+  )
+
+list(APPEND _cmake_import_check_targets GTest::gtest )
+list(APPEND _cmake_import_check_files_for_GTest::gtest "${_IMPORT_PREFIX}/lib/gtest.lib" )
+
+# Import target "GTest::gtest_main" for configuration "Release"
+set_property(TARGET GTest::gtest_main APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(GTest::gtest_main PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/gtest_main.lib"
+  )
+
+list(APPEND _cmake_import_check_targets GTest::gtest_main )
+list(APPEND _cmake_import_check_files_for_GTest::gtest_main "${_IMPORT_PREFIX}/lib/gtest_main.lib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
