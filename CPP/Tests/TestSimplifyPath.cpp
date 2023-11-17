7a481e87aac279b10da717872bcebd740c0d03f3
Klaus Loft Højbjerre <10060520+klauslh@users.noreply.github.com>
2023-11-17 19:28:00 +0100

    Fix simplification of open path (#714)
    * Add failing simplification test
    
    A slightly wiggly line from (0,0) to (0,100), which we expect to
    simplify to a straight line of length 100 with just the two endpoints
    remaining.
    
    Instead it simplifies to a much shorter line.
    
    * Don't recompute distance for open endpoint

---
+++ CPP/Tests/TestSimplifyPath.cpp
@@ -0,0 +1,14 @@
+#include "clipper2/clipper.h"
+#include <gtest/gtest.h>
+
+using namespace Clipper2Lib;
+
+TEST(Clipper2Tests, TestSimplifyPath) {
+
+  Path64 input1 = MakePath({
+      0,0, 1,1, 0,20, 0,21, 1,40, 0,41, 0,60, 0,61, 0,80, 1,81, 0,100
+  });
+  Path64 output1 = SimplifyPath(input1, 2, false);
+  EXPECT_EQ(Length(output1), 100);
+  EXPECT_EQ(output1.size(), 2);
+}
