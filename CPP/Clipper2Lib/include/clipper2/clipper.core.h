320af5b8a1c8e2af81e2ace20576ae67d9dd675f
Richard Barnes <rbarnes@umn.edu>
2024-07-15 10:56:52 -0700

    throw() -> noexcept
    `throw()` was deprecated in C++11 and removed in C++17. `noexcept` is the appropriate fix.

--- CPP/Clipper2Lib/include/clipper2/clipper.core.h
+++ CPP/Clipper2Lib/include/clipper2/clipper.core.h
@@ -31,7 +31,7 @@ namespace Clipper2Lib
   public:
     explicit Clipper2Exception(const char* description) :
       m_descr(description) {}
-    virtual const char* what() const throw() override { return m_descr.c_str(); }
+    virtual const char* what() const noexcept override { return m_descr.c_str(); }
   private:
     std::string m_descr;
   };
