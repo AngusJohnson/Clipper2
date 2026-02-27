c461ef2cf6677ea0800e3cdb60526efdcb231e4d
Anaxarchus <anaxarch@protonmail.com>
2026-02-27 00:59:32 -0900

    fixed incorrect include path, added cmake switch for emcc
    Tested on:
    MacBook Air
    chip: Apple M2
    macOS: Tahoe 26.3

--- CPP/Clipper2Lib/src/clipper.export.cpp
+++ CPP/Clipper2Lib/src/clipper.export.cpp
@@ -1,4 +1,4 @@
-#include "Clipper2Lib/include/clipper2/clipper.h"
-#include "Clipper2Lib/include/clipper2/clipper.export.h"
+#include "clipper2/clipper.h"
+#include "clipper2/clipper.export.h"
 
 using namespace Clipper2Lib;
